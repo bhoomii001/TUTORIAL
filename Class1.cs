c34260730dca2a27abdc08f58858abf5e46e1042
Bhoomi <bhoomiahir321@gmail.conm>
2025-07-01 15:43:31 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTORIAL
+{
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hiiii");
+            Console.Read();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace TUTORIAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the number");
+            int a=Convert.ToInt32(Console.ReadLine());
+            
+
+            if(a%2==0)
+            {
+                Console.WriteLine("it is even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+            Console.Read();
+        }
+    }
+}
